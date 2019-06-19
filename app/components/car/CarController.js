44998c344c89fb2e407dc42a085087ac97d9f8c7
Darryl Kilzer <darrylkilzer@gmail.com>
2019-06-19 11:28:41 -0600

    template created, cars are drawing

--- app/components/car/CarController.js
+++ app/components/car/CarController.js
@@ -3,8 +3,20 @@ import CarService from "./CarService.js";
 
 let _carService = new CarService()
 
+function drawCars(){
+  let carsElem = document.querySelector("#cars")
+  let template = ''
+  let cars = _carService.Cars
+  cars.forEach(car => {
+    template += car.Template
+  })
+  carsElem.innerHTML = template
+
+}
+
 export default class CarController{
   constructor(){
     console.log("car controller works")
+    drawCars()
   }
 }
--- app/components/car/CarService.js
+++ app/components/car/CarService.js
@@ -1,11 +1,18 @@
 import Car from "../../models/Car.js";
 
 
-let _cars = [new Car("Buick", "Century", 1989, "Cherry")]
+let _cars = [
+  new Car({make:"Buick", model:"Century", year:1989, color:"Cherry"}),
+  new Car({make:"Lambo", model:"diablo", year:2006, color:"Canary Yellow"})
+]
 
 
 export default class CarService{
   constructor(){
     console.log("car service works", _cars)
+  }
+
+  get Cars(){
+    return _cars.map(car => new Car(car))
   }
 }
